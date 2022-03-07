3fd13cadb07467c41fef9a3d40c014c8f2599e4c
adam <dev@adamjayne.org>
2022-03-07 13:01:54 -0500

    completed infrastructure for s3 deployment of a static site SPA

--- lib/cloud-resume-cdk-stack.ts
+++ lib/cloud-resume-cdk-stack.ts
@@ -1,16 +1,59 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+declare const hostedZone: route53.HostedZone;
 
 export class CloudResumeCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CloudResumeCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // const resumeCertificate = new acm.DnsValidatedCertificate(this, 'resume-site-cert', {
+    //   domainName: 'www.adamljayne.com',
+    //   hostedZone,
+    //   subjectAlternativeNames: [
+    //     'test.adamljayne.com',
+    //   ]
     // });
+
+    let resumeBucket = new s3.Bucket(this, 'ResumeBucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html', 
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+
+    new s3deploy.BucketDeployment(this, 'ResumeClientBucketDeployment', {
+      sources: [s3deploy.Source.asset('./lib/client')],
+      destinationBucket: resumeBucket
+    });
+
+    new CfnOutput(this, 'WebsiteUrl', {
+      value: resumeBucket.bucketWebsiteUrl
+    });
+
+    // new cloudfront.Distribution(this, 'ResumeDistrobution', {
+    //   defaultBehavior: {
+    //     origin: new origins.S3Origin(resumeBucket),
+    //   },
+    //   certificate: resumeCertificate
+    // });
+
+    // new route53.ARecord(this, 'resumeS3Domain', {
+    //   zone: hostedZone,
+    //   target: route53.RecordTarget.fromAlias(resumeBucket.urlForObject)
+    // });
+
+
+    
   }
 }
--- test/cloud-resume-cdk.test.ts
+++ test/cloud-resume-cdk.test.ts
@@ -1,17 +1,20 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CloudResumeCdk from '../lib/cloud-resume-cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as CloudResumeCdk from '../lib/cloud-resume-cdk-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/cloud-resume-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
+test('S3 Bucket Created', () => {
+  const app = new cdk.App();
 //     // WHEN
-//   const stack = new CloudResumeCdk.CloudResumeCdkStack(app, 'MyTestStack');
+  const stack = new CloudResumeCdk.CloudResumeCdkStack(app, 'MyTestStack');
 //     // THEN
-//   const template = Template.fromStack(stack);
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::S3::Bucket', {
+    WebsiteConfiguration: {
+      IndexDocument: 'index.html',
+      ErrorDocument: 'index.html'
+    }
+  });
 });
