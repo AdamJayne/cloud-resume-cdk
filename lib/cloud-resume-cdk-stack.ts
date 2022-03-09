df85830f392cf90b766e021b0faad7e3464d999e
adam <dev@adamjayne.org>
2022-03-09 13:21:19 -0500

    Pipeline completed, including notes as well as formatted the documents

--- lib/cloud-resume-cdk-stack.ts
+++ lib/cloud-resume-cdk-stack.ts
@@ -1,77 +1,105 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
-import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
-import * as dynamoDB from 'aws-cdk-lib/aws-dynamodb';
-import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
-import * as path from 'path';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
+import * as dynamoDB from "aws-cdk-lib/aws-dynamodb";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
+import * as pipelines from "aws-cdk-lib/pipelines";
+import * as path from "path";
 
-import { StaticWebApplication } from './constructs/client-bucket';
+import { StaticWebApplication } from "./constructs/client-bucket";
 
 export class CloudResumeCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const baseDomain = 'adamljayne.com';
-    const websiteDomain = `resume.${ baseDomain }`;
-    const apiDomain = `resumeapi.${ baseDomain }`;
+    // Front-end and back-end domains to use for the application
+    const baseDomain = "adamljayne.com";
+    const websiteDomain = `resume.${baseDomain}`;
+    const apiDomain = `resumeapi.${baseDomain}`;
 
-    const hostedZone = route53.HostedZone.fromLookup(this, "DomainZone", { domainName: baseDomain });
-
-    const certificate = new acm.DnsValidatedCertificate(this, `-WebApplicationCertificate`, {
-      domainName: `*.${ baseDomain }`,
-      cleanupRoute53Records: true,
-      hostedZone
+    // Pulling the hosted zone for the application's domain
+    const hostedZone = route53.HostedZone.fromLookup(this, "DomainZone", {
+      domainName: baseDomain,
     });
+
+    // Generating an SSL certificate for all subdomains in the domain chosen, using automatic Route53 DNS validation
+    const certificate = new acm.DnsValidatedCertificate(
+      this,
+      `-WebApplicationCertificate`,
+      {
+        domainName: `*.${baseDomain}`,
+        cleanupRoute53Records: true,
+        hostedZone,
+      }
+    );
 
     // This was done to test my understanding of how the Construct class works
     // The StaticWebApplication class represents the creation and deployment of a static web app
-    new StaticWebApplication(this, 'Resume', {
+    new StaticWebApplication(this, "Resume", {
       domainName: websiteDomain,
-      webAppDirectory: path.join(__dirname, '/client'),
-      indexDocument: 'resume.html',
-      errorDocument: 'resume.html',
+      webAppDirectory: path.join(__dirname, "/client"),
+      indexDocument: "resume.html",
+      errorDocument: "resume.html",
       hostedZone,
       acmCertificate: certificate,
     });
 
-    // DynamoDB Instance
-    const resumeDatabase = new dynamoDB.Table(this, 'ResumeTable', {
-      tableName: 'test-table',
-      partitionKey: { name: 'id', type: dynamoDB.AttributeType.STRING },
-      billingMode: dynamoDB.BillingMode.PAY_PER_REQUEST
+    // Create the table in DynamoDB the application will use
+    const resumeDatabase = new dynamoDB.Table(this, "ResumeTable", {
+      tableName: "test-table",
+      partitionKey: { name: "id", type: dynamoDB.AttributeType.STRING },
+      billingMode: dynamoDB.BillingMode.PAY_PER_REQUEST,
     });
 
-    // Lambda Function with API Gateway
-    const resumeViewsFunction = new Function(this, 'ResumeViewCountFunction', {
+    // Create a lambda function using the code inside of the /lambda directory.
+    const resumeViewsFunction = new Function(this, "ResumeViewCountFunction", {
       runtime: Runtime.NODEJS_14_X,
-      handler: 'page-views.handler',
-      code: Code.fromAsset(path.join(__dirname, 'lambda'))
+      handler: "page-views.handler",
+      code: Code.fromAsset(path.join(__dirname, "lambda")),
     });
 
     // Granting the Lambda function READ/WRITE to the DynamoDB Instance
     resumeDatabase.grantReadWriteData(resumeViewsFunction);
 
-    const resumeApiGateway = new apiGateway.RestApi(this, 'ResumeApiGateway', {
-      restApiName: 'Resume Api Test',
-      description: 'Testing purposes',
+    // Creating an api gateway for the backend
+    const resumeApiGateway = new apiGateway.RestApi(this, "ResumeApiGateway", {
+      restApiName: "Resume Api Test",
+      description: "Testing purposes",
       domainName: {
         domainName: apiDomain,
-        certificate
-      }
+        certificate,
+      },
     });
 
-    const resumeViewsLambdaGatewayIntegration = new apiGateway.LambdaIntegration(resumeViewsFunction);
+    // Generating a gateway integration with the lambda function
+    const resumeViewsLambdaGatewayIntegration =
+      new apiGateway.LambdaIntegration(resumeViewsFunction);
 
-    resumeApiGateway.root.addMethod('GET', resumeViewsLambdaGatewayIntegration);
+    // Attaching that gateway integration to the base route in the gateway
+    resumeApiGateway.root.addMethod("GET", resumeViewsLambdaGatewayIntegration);
 
-    new route53.ARecord(this, 'ResumeApiGatewayCustomDomainRecord', {
+    // Creating an A record for the api gateway domain for a custom domain
+    new route53.ARecord(this, "ResumeApiGatewayCustomDomainRecord", {
       zone: hostedZone,
       recordName: apiDomain,
-      target: route53.RecordTarget.fromAlias(new route53Targets.ApiGateway(resumeApiGateway))
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.ApiGateway(resumeApiGateway)
+      ),
     });
 
+    // Create a code pipeline for CI/CD when the main branch is updated on GitHub
+    new pipelines.CodePipeline(this, "ResumeApplicationCodePipeline", {
+      pipelineName: "ResumeAppPipeline",
+      synth: new pipelines.ShellStep("CDK Synth", {
+        input: pipelines.CodePipelineSource.gitHub(
+          "AdamJayne/resume-challenge-cdk",
+          "main"
+        ),
+        commands: ["npm ci", "npm run build", "npm cdk synth"],
+      }),
+    });
   }
 }
--- lib/constructs/client-bucket.ts
+++ lib/constructs/client-bucket.ts
@@ -1,52 +1,57 @@
 import { RemovalPolicy } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as acm from 'aws-cdk-lib/aws-certificatemanager';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as route53 from "aws-cdk-lib/aws-route53";
-import * as s3 from 'aws-cdk-lib/aws-s3';
-import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as targets from 'aws-cdk-lib/aws-route53-targets'
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as targets from "aws-cdk-lib/aws-route53-targets";
 
 export interface StaticWebApplicationProps {
-  domainName: string,
-  webAppDirectory: string,
-  indexDocument: string,
-  errorDocument: string,
-  hostedZone: route53.IHostedZone,
-  acmCertificate: acm.ICertificate
+  domainName: string;
+  webAppDirectory: string;
+  indexDocument: string;
+  errorDocument: string;
+  hostedZone: route53.IHostedZone;
+  acmCertificate: acm.ICertificate;
 }
 
 export class StaticWebApplication extends Construct {
-
   constructor(scope: Construct, id: string, props: StaticWebApplicationProps) {
     super(scope, id);
 
     const bucket = new s3.Bucket(this, `-WebApplicationBucket`, {
       publicReadAccess: true,
       websiteIndexDocument: props.indexDocument,
-      websiteErrorDocument: props.errorDocument, 
+      websiteErrorDocument: props.errorDocument,
       removalPolicy: RemovalPolicy.DESTROY,
-      autoDeleteObjects: true
+      autoDeleteObjects: true,
     });
 
     new s3deploy.BucketDeployment(this, `-WebApplicationBucketDeployment`, {
       sources: [s3deploy.Source.asset(props.webAppDirectory)],
-      destinationBucket: bucket
+      destinationBucket: bucket,
     });
 
-    const distrobution = new cloudfront.Distribution(this, `-WebApplicationDistrobution`, {
-      defaultBehavior: {
-        origin: new origins.S3Origin(bucket),
-      },
-      domainNames: [props.domainName],
-      certificate: props.acmCertificate
-    });
+    const distrobution = new cloudfront.Distribution(
+      this,
+      `-WebApplicationDistrobution`,
+      {
+        defaultBehavior: {
+          origin: new origins.S3Origin(bucket),
+        },
+        domainNames: [props.domainName],
+        certificate: props.acmCertificate,
+      }
+    );
 
     new route53.ARecord(this, `-WebApplicationCloudfrontRecord`, {
       zone: props.hostedZone,
       recordName: props.domainName,
-      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distrobution))
+      target: route53.RecordTarget.fromAlias(
+        new targets.CloudFrontTarget(distrobution)
+      ),
     });
   }
 }
